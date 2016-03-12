c1a48fb9de581909bd4360da48347492da0394f4
Henry Du <henry.du@live.com>
2016-03-12 01:14:41 -0500

    added post endpoint

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
@@ -9,4 +13,9 @@ app.use('/home', express.static('index.html'));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
+});
+
+app.post('/identifai', function(req, res) {
+	console.log(req.body.hello);
+	res.send("dick");
 });
